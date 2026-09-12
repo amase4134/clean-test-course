94eb1e2c9507990d81a4a5e8c4c557f8fefae67b
Mase <amase4134@gmail.com>
2026-09-12 15:30:00 -0400

    adding integration tests

--- frontend/src/pages/Home/Home.test.js
+++ frontend/src/pages/Home/Home.test.js
@@ -51,6 +51,21 @@ describe('Test Home', () => {
     //The word Appeateasers should be in there as defined in the mock response above.
     expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
   });
+  
+  test('Test Render', async () => {
+    //Arrange: Setup the mock API
+    //Listen for any GET requests using the axios module
+    //Intercept the GET requests and provide a mocked response
+
+    //Act: Call the Home page
+    render(<Home />);
+
+    //Assert: Check the values in the rendered Home page.
+    //There should be 2 categories as defined in the mock response above
+    expect(await screen.findAllByTestId(/category-item/i)).toHaveLength(2);
+    //The word Appeateasers should be in there as defined in the mock response above.
+    expect(await screen.findByText('Appeteasers')).toBeInTheDocument();
+  });
 
   test('Negative Test: Test Failed Category call', async () => {
     //Arrange: Setup the mock API
